e9be1673ae5f041fcda1b0f049cdec74cafd60bf
Alexander <60114847+sigdevel@users.noreply.github.com>
2025-02-18 15:27:26 +0300

    Update parse.h

--- parse.h
+++ parse.h
@@ -1,23 +1,27 @@
+#ifndef PARSE_H
+#define PARSE_H
+
 #include <stdio.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 #define parameter_array_size 100
-#define parameter_strings_long 100
-#define file_parameter_strings_long 200
-#define variable_array_size 30
+#define variable_array_size 50
+#define parameter_strings_long 256
 
-struct parameter_list
-{
-    _Bool must;
+typedef struct {
+    int must;
     int count;
     char parameter[variable_array_size][parameter_strings_long];
-};
+} ParameterType;
 
-char file_parameter[file_parameter_strings_long];
-struct parameter_list parameter[parameter_array_size];
-int parameter_count;
+extern int parameter_count;
+extern char file_parameter[parameter_strings_long];
+extern ParameterType parameter[parameter_array_size];
 
 void parse_xml(char *xml_posion);
+void print_parse_result();
+
+#endif //PARSE_H
